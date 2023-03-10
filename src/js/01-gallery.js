7d4cbdd71c510c6f2ef457ec3449625dc11f2b3b
Oleksandr-Kharkiv <est_@ukr.net>
2023-03-10 22:16:48 +0100

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
